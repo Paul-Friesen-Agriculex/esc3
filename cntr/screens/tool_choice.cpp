2f9af57c58436659683899e482a0187d096823e7
Paul Friesen <paulfriesenagriculex@gmail.com>
2017-11-08 18:52:10 +0000

    version 2023-06-16

--- cntr/screens/tool_choice.cpp
+++ cntr/screens/tool_choice.cpp
@@ -111,7 +111,7 @@ void tool_choice::about_button_clicked()
   help_screen_p = new help_screen;
   help_screen_p -> setGeometry(geometry());
   
-  help_screen_p -> set_text("AGRICULEX ESC-3 PROGRAM VERSION 2023-02-28\n\n"
+  help_screen_p -> set_text("AGRICULEX ESC-3 PROGRAM VERSION 2023-06-16\n\n"
   
                             "Copyright 2023 Agriculex Inc.\n\n"
 
