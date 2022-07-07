d2ef9ae0ab8e0ed4912d458429e0f678fb4a722d
Paul-Friesen-Agriculex <paulfriesenagriculex@gmail.com>
2022-07-07 13:59:28 +0000

    licensing added

--- cntr/screens/crop_edit.hpp
+++ cntr/screens/crop_edit.hpp
@@ -1,3 +1,28 @@
+/***********
+
+Copyright 2021 Agriculex Inc.
+
+This file is part of the Agriculex ESC-3 program.
+
+The Agriculex ESC-3 program is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General Public License as 
+published by the Free Software Foundation, GPL-3.0-or-later.
+
+The Agriculex ESC-3 program is distributed in the hope that it will be 
+useful, but, except as noted below, WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with The Agriculex ESC-3 program.  If not, see 
+<https://www.gnu.org/licenses/>.
+
+Agriculex Inc. warrants that executable files of the ESC-3 program 
+compiled and installed by Agriculex Inc. will function as advertized in 
+ESC-3 machines produced by Agriculex Inc.
+
+**********/
+
 //-------------------------------------------------------------------------------------------------------
 //Class name: crop_edit
 //Purpose: providing an ability to add new crop, delete exist crop or revise parameters of existing crop
